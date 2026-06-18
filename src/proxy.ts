20a1efdc20554869b9a8169c05824b7ae01c2508
Tanveer-UIUX <tanveer@codingcrafts.io>
2026-06-18 18:31:51 +0500

    proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,17 +1,17 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const authHeader = req.headers.get('authorization');
 
   const USERNAME = 'admin';
-  const PASSWORD = 'Solveye77@#23'; 
+  const PASSWORD = 'Solveye77@#23';
 
   if (authHeader) {
     try {
       const auth = authHeader.split(' ')[1];
       const decoded = atob(auth).split(':');
-      
+
       if (decoded[0] === USERNAME && decoded[1] === PASSWORD) {
         return NextResponse.next();
       }
@@ -28,7 +28,6 @@ export function middleware(req: NextRequest) {
   });
 }
 
-// This tells Next.js to protect every single page on your site
 export const config = {
   matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 };
